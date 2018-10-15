80d3c1dc724c8b4c5159755dbbe65268ea8a2116
nillbabu <you@example.com>
2018-10-15 15:29:10 +0600

    Updates

---
+++ precache-manifest.31d7780fba87a37e298ebc57c339cae1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bc0e53f2b3e7e33c4b5",
+    "url": "/Indecision-App/static/js/runtime~main.8bc0e53f.js"
+  },
+  {
+    "revision": "5f34b86d7e379c0a687f",
+    "url": "/Indecision-App/static/js/main.5f34b86d.chunk.js"
+  },
+  {
+    "revision": "c3c10070700ec3d9c080",
+    "url": "/Indecision-App/static/js/1.c3c10070.chunk.js"
+  },
+  {
+    "revision": "5f34b86d7e379c0a687f",
+    "url": "/Indecision-App/static/css/main.1ad5106f.chunk.css"
+  },
+  {
+    "revision": "c3c10070700ec3d9c080",
+    "url": "/Indecision-App/static/css/1.9e70de05.chunk.css"
+  },
+  {
+    "revision": "0eb14ceaba7c6b1efdc9c81ed7f5caca",
+    "url": "/Indecision-App/index.html"
+  }
+];
